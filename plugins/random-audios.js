2d8ffb856d94cb92345af5a460d27b668fead9b5
Juan1986 <166744425+ByJuan1986@users.noreply.github.com>
2026-02-07 16:37:51 +0100

    Add audio URL for 'insecto' regex match

--- plugins/random-audios.js
+++ plugins/random-audios.js
@@ -21,6 +21,7 @@ const audioMap = [
 // Audio en latino 
 {regex: /^carita empapada$/i, url: 'https://od.lk/s/NTZfMjg3MzAzNzZf/Carita%20empapada.mp3'},  
 {regex: /^cell tiene miedo$/i, url: 'https://od.lk/s/NTZfMjkyMjY5MjVf/C%C3%A9lula%20tuve%20miedo.mp3'},
+{regex: /^insecto$/i, url: 'https://od.lk/s/NTZfMjkwMDk0NDFf/Goku%20latino%20insecto.mp3'},
 
 ]
 
