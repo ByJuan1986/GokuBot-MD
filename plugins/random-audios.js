ad9593dc7354c1e48d98f2ef9d5e161123bf105e
Juan1986 <166744425+ByJuan1986@users.noreply.github.com>
2026-02-08 08:47:31 +0100

    Update random-audios.js

--- plugins/random-audios.js
+++ plugins/random-audios.js
@@ -12,13 +12,18 @@ if (!db.data.chats[m.chat].fAudios) return
 const audioMap = [
 {regex: /^Bien pensado boddy|bien pensado$/i, url: 'https://files.catbox.moe/cv4969.mp3'},
 {regex: /^soy toxico|les saluda el toxico$/i, url: 'https://files.catbox.moe/pxgxpk.mp3'},
+
+// GokuBot castellano
+{regex: /^buenas noches|que descanses$/i, url: 'https://od.lk/s/NTZfMjkxOTY1NjNf/buenas%20noches%20que%20descanses%20castellano.mp3'},
+ 
+ 
+ // GokuBot latino
 {regex: /^buenas noches bro$/i, url: 'https://files.catbox.moe/wsgu0d.aac'},
 {regex: /^buenos dias$/i, url: 'https://od.lk/s/NTZfMjkxOTY4ODlf/buenos%20d%C3%ADas%20Goku%20latino.mp3'},  
 {regex: /^hola GokuBot$/i, url: 'https://od.lk/s/NTZfMjg3MzA5NDFf/GokuBot%20hola%20latino.mp3'},
-{regex: /^kame haaa$/i, url: 'https://files.catbox.moe/g2r6ww.mp3'},
-{regex: /^Goku eta vaina es seria$/i, url: 'https://od.lk/s/NTZfMjk4MTU0NTNf/Esta%20vaina%20es%20seria.mp3'},
 
 // Audio en latino 
+{regex: /^Goku eta vaina es seria$/i, url: 'https://od.lk/s/NTZfMjk4MTU0NTNf/Esta%20vaina%20es%20seria.mp3'},
 {regex: /^carita empapada$/i, url: 'https://od.lk/s/NTZfMjg3MzAzNzZf/Carita%20empapada.mp3'},  
 {regex: /^cell tiene miedo$/i, url: 'https://od.lk/s/NTZfMjkyMjY5MjVf/C%C3%A9lula%20tuve%20miedo.mp3'},
 {regex: /^insecto$/i, url: 'https://od.lk/s/NTZfMjkwMDk0NDFf/Goku%20latino%20insecto.mp3'},
