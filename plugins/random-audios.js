15775bf3d13f40178af9f69368a76daf8d2c3481
Juan1986 <166744425+ByJuan1986@users.noreply.github.com>
2026-02-09 18:34:18 +0100

    Update random-audios.js

--- plugins/random-audios.js
+++ plugins/random-audios.js
@@ -79,7 +79,6 @@ const audioMap = [
 {regex: /^que viene$/i, url: 'https://od.lk/s/NTZfMjg3Mjg2NDNf/Amador%20Ribas%20-%20Que%20viene%20que%20viene.mp3'},
 {regex: /^salami$/i, url: 'https://od.lk/s/NTZfMjg3Mjg2NDRf/Amador%20Ribas%20-%20Salami.mp3'},
 {regex: /^te voy a poner fina$/i, url: 'https://od.lk/s/NTZfMjg3Mjg2NDVf/Amador%20Ribas%20-%20Te%20voy%20a%20poner%20fina%20filipina.mp3'},
-{regex: /^meminas la moral$/i, url: 'https://od.lk/d/NTZfMjg3Mjg2Mzhf/Antonio%20Recio%20-%20Meminas%20la%20moral.mp3'},
 {regex: /^que culito tengo$/i, url: 'https://od.lk/s/NTZfMjg3Mjg2NDZf/Antonio%20Recio%20-%20Que%20culito%20tengo.mp3'},
  
 ]
