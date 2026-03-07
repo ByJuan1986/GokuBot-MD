3efa4e35abf6cd26627de6bdb7b34dc2b286e26e
Juan1986 <166744425+ByJuan1986@users.noreply.github.com>
2026-03-07 20:16:20 +0100

    Fix regex for 'me están mirando' audio trigger

--- plugins/random-audios.js
+++ plugins/random-audios.js
@@ -56,7 +56,7 @@ const audioMap = [
 {regex: /^eres un cobarde$/i, url: 'https://od.lk/s/NTZfMjg3NDcyMTZf/meme%208%20-%20t%C3%BA%20eres%20un%20cobarde.mp3'},    
 {regex: /^ya basta$/i, url: 'https://od.lk/s/NTZfMjg3NDcyNDdf/meme%209%20-%20ya%20basta%20Freezer.mp3'},
 {regex: /^mi dinero$/i, url: 'https://od.lk/s/NTZfMjkyMjY5NDFf/mi%20dinero%20disminulle.mp3'},
-{regex: /^joder|me estan mirando$/i, url: 'https://od.lk/s/NTZfNDc5MDI1Njdf/Se%20estan%20riendo%20de%20mi.mp3'},
+{regex: /^joder|me están mirando|me estan mirando$/i, url: 'https://od.lk/s/NTZfNDc5MDI1Njdf/Se%20estan%20riendo%20de%20mi.mp3'},
 
  // Audio en latino
 {regex: /^hay alguien en el grupo$/i, url: 'https://od.lk/s/NTZfMjg3MzAzNzdf/Hay%20alguien%20en%20el%20grupo.mp3'},
