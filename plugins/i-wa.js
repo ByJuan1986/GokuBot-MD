dfea256ed50a1261b81d6069e498845d9bff0f4d
Juan1986 <166744425+ByJuan1986@users.noreply.github.com>
2026-06-23 17:33:06 +0200

    Update image URL in sendMessage function

--- plugins/i-wa.js
+++ plugins/i-wa.js
@@ -526,7 +526,7 @@ let random = `𝗧𝗲 𝗺𝗼𝘀𝘁𝗿𝗮𝗺𝗼𝘀 𝘂𝗻 𝗹𝗶�
 • 
 • 
 `
-await conn.sendMessage(m.chat, { image: { url: "https://od.lk/s/NTZfNDgwNjUwMTJf/reglas.jpeg" }, caption: random }, { quoted: m })
+await conn.sendMessage(m.chat, { image: { url: "https://od.lk/s/NTZfNTcwMjUwMTBf/Menubiograf%C3%ADa.jpg" }, caption: random }, { quoted: m })
 }
 
 
