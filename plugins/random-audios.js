3a5f6abcb82f83f489c30e6f94b4755cc4c9c767
Juan1986 <166744425+ByJuan1986@users.noreply.github.com>
2026-09-13 10:37:24 +0200

    Fix URL for DB ending latino audio

--- plugins/random-audios.js
+++ plugins/random-audios.js
@@ -90,7 +90,7 @@ const audioMap = [
  
 // BSO dragon ball latino  
 {regex: /^DB opening latino$/i, url: 'https://od.lk/s/NTZfNDg0ODQ4MTVf/DB%20opening%20latino.mp3'},
-{regex: /^DB ending latino$/i, url: 'https://od.lk/s/NTZfNTgyNjcxNDlf/DB%20ending%20Latino.mp3'},
+{regex: /^DB ending latino$/i, url: 'https://od.lk/s/NTZfNTgyNjcxNDlfZzlmZlE/DB%20ending%20Latino.mp3'},
 
  
 // Personajes dragon ball en latino
